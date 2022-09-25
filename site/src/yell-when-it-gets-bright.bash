a732cb40167e1e86560464593d55c4aa523e59f3
Alan Smith <commit@alanwsmith.com>
2022-09-25 18:47:35 -0400

    Updated to single file

--- site/src/yell-when-it-gets-bright.bash
+++ site/src/yell-when-it-gets-bright.bash
@@ -1,26 +1,41 @@
 #!/bin/bash
 
-ALERT_THROTTLE=0
+trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
-fswatch -0 images/ | while read -d "" event
+ffmpeg -f avfoundation \
+    -hide_banner \
+    -loglevel panic \
+    -i "1:0" \
+    -vf "fps=5,scale=100:-2" \
+    -y "images/%04d.png" &
+
+THROTTLE=0
+
+# fswatch -l 0.1 -m poll_monitor -0 images/ | while read -d "" event
+fswatch -l 0.1 -0 images/ | while read -d "" event
 do 
     if [ -e "$event" ]
     then
-        LIGHT_LEVEL_FLOAT=`convert "$event" -colorspace gray -format "%[fx:100*mean]" info:`
-        LIGHT_LEVEL_INT=`printf %.0f "$LIGHT_LEVEL_FLOAT"`
-        echo $LIGHT_LEVEL_INT
-        if [ $LIGHT_LEVEL_INT -gt 40 ]
+        BRIGHT_F=`convert "$event" \
+            -colorspace gray \
+            -format "%[fx:100*mean]" \
+            info:`
+        BRIGHT_I=`printf %.0f "$BRIGHT_F"`
+        echo -n "$BRIGHT_I "
+        if [ $BRIGHT_I -gt 40 ]
         then
-            if [ $ALERT_THROTTLE -eq 0 ]
-            then
-                ALERT_THROTTLE=1
-                afplay ahhhh.mp3
-                echo "YELL"
-            fi
-        else
-            ALERT_THROTTLE=0
-        fi
-        rm "$event"
+             if [ $THROTTLE -eq 0 ]
+             then
+                 THROTTLE=1
+                 afplay ahhhh.mp3 &
+                 echo "YELL"
+             fi
+         else
+             THROTTLE=0
+         fi
+         rm "$event"
     fi
 done
+
+wait 
 
